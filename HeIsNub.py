572f875a064a6b098d165c00bd3da727ffd0dd04
AaronWagner27 <sl4yerkidkthnxbai@gmail.com>
2017-11-10 11:37:40 -0500

    Killed an animal
    It was py

---
+++ HeIsNub.py
@@ -0,0 +1,6 @@
+ChristianIsNub = ["C","H","R","I","S","T","I","A","N"," ","I","S"," ","A"," ","N","U","B"]
+letters = 0
+
+for letters in range(len(ChristianIsNub)):
+    print(ChristianIsNub[letters])
+    letters = letters + 1
