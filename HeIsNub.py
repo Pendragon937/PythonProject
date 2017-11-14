bfd3134a06004d91d2152446ade35f6b3398ced3
AaronWagner27 <sl4yerkidkthnxbai@gmail.com>
2017-11-14 14:15:39 -0500

    del shiz

--- HeIsNub.py
+++
@@ -1,6 +0,0 @@
-ChristianIsNub = ["C","H","R","I","S","T","I","A","N"," ","I","S"," ","A"," ","N","U","B"]
-letters = 0
-
-for letters in range(len(ChristianIsNub)):
-    print(ChristianIsNub[letters])
-    letters = letters + 1
