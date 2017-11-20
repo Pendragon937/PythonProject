8fe1a9542fd5fd42d421424d13b7edf0cc41bb56
AbidFAli <abidfiazali@gmail.com>
2017-11-20 13:45:06 -0500

    Bullet Shooting

--- snake_gui.py
+++ snake_gui.py
@@ -21,6 +21,24 @@ def rot_center(image,angle):
     rot_image = rot_image.subsurface(rot_rect).copy()
     return rot_image
 
+class SpaceShip(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = load_image('Ship.png')
+        self.rect = self.image.get_rect()
+        self.angle = 0
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface([4,10])
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+        self.angle = 0
+    def update(self):
+        self.rect.y += 5* math.sin(math.radians(-1*self.angle - 90))
+        self.rect.x += 5* math.cos(math.radians(-1*self.angle - 90))
 
 def main():
     pygame.init() #initializes pygame
@@ -30,10 +48,15 @@ def main():
     done = False
     clock = pygame.time.Clock()
     original = load_image('Ship.png')
-    ship = load_image('Ship.png')
-    center = [500, 500]
-    angle = 0
+    bullet_list = pygame.sprite.Group()
+    ship = SpaceShip()
+    screen.fill(WHITE)
+    ship.rect.x = 500
+    ship.rect.y = 500
+    screen.blit(ship.image,ship.rect)
+    pygame.display.flip()
 
+    # bull = Bullet()
 
     # Event loop
     while not done:
@@ -42,26 +65,28 @@ def main():
                 done = True
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    angle += 15
-                    ship = rot_center(original, angle)
+                    ship.angle += 15
+                    ship.image = rot_center(original,ship.angle)
                 elif event.key == pygame.K_RIGHT:
-                    angle -= 15
-                    ship = rot_center(original, angle)
-
+                    ship.angle -= 15
+                    ship.image = rot_center(original,ship.angle)
+                elif event.key == pygame.K_SPACE:
+                    bullet = Bullet()
+                    bullet.angle = ship.angle
+                    bullet.rect.x = ship.rect.x
+                    bullet.rect.y = ship.rect.y
+                    bullet_list.add(bullet)
 
         screen.fill(WHITE)
-        screen.blit(ship,(500,500))
+        screen.blit(ship.image,(500,500))
 
-        #Asteroid testing
+        bullet_list.update()
+        bullet_list.draw(screen)
         Asteroid = load_image('Asteroid.png')
-        Asteroid2 = load_image('Asteroid2.png')
-        Asteroid3 = load_image('Asteroid3.png')
         screen.blit(Asteroid, (900,500))
-        screen.blit(Asteroid2, (100,500))
-        screen.blit(Asteroid3, (500,100))
+        screen.blit(Asteroid, (100,500))
+        screen.blit(Asteroid, (500,100))
         screen.blit(Asteroid, (500,900))
-
-        
         # pygame.draw.line(screen,RED,[500,500],[525,525],5)
         # pygame.draw.line(screen,RED,[500,500],[475,525],5)
         # pygame.draw.line(screen,RED,[525,525],[475,525],5)
