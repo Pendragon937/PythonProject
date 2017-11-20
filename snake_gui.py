64b09d4d17fdf7793eb433a0ed7c9475531d587d
AbidFAli <abidfiazali@gmail.com>
2017-11-20 15:14:28 -0500

    Holding Keys

--- snake_gui.py
+++ snake_gui.py
@@ -28,6 +28,24 @@ class SpaceShip(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.angle = 0
 
+class Asteroid(pygame.sprite.Sprite):
+    def __init__(self,ast_size):
+        super().__init__()
+        self.ast_size = ast_size
+        if ast_size == 'Big':
+            self.image = load_image('Asteroid.png')
+        elif ast_size == 'Medium':
+            self.image = load_image('Asteroid2.png')
+        else:
+            self.image = load_image('Asteroid3.png')
+        self.rect = self.image.get_rect()
+        self.xspeed = 5
+        self.yspeed = 5
+
+    def update(self):
+        self.rect.x += self.xspeed
+        self.rect.y += self.yspeed
+
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self):
@@ -42,54 +60,77 @@ class Bullet(pygame.sprite.Sprite):
 
 def main():
     pygame.init() #initializes pygame
-    screen = pygame.display.set_mode((1000,1000))
+    screen = pygame.display.set_mode((800,800))
     pygame.display.set_caption('Asteroid')
 
     done = False
     clock = pygame.time.Clock()
     original = load_image('Ship.png')
-    bullet_list = pygame.sprite.Group()
-    ship = SpaceShip()
     screen.fill(WHITE)
-    ship.rect.x = 500
-    ship.rect.y = 500
-    screen.blit(ship.image,ship.rect)
-    pygame.display.flip()
 
-    # bull = Bullet()
+    bullet_list = pygame.sprite.Group()
+    asteroid_list = pygame.sprite.Group()
+    sprite_list = pygame.sprite.Group()
+
+    ship = SpaceShip()
+    ship.rect.x = 400
+    ship.rect.y = 400
+    screen.blit(ship.image,ship.rect)
+
+    # ast = Asteroid('Small')
+    # ast.rect.x = 100
+    # ast.rect.y = 100
+    # asteroid_list.add(ast)
+    # asteroid_list.draw(screen)
+
+    pygame.display.flip()
 
     # Event loop
     while not done:
+        keys = pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == QUIT:
                 done = True
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    ship.angle += 15
-                    ship.image = rot_center(original,ship.angle)
-                elif event.key == pygame.K_RIGHT:
-                    ship.angle -= 15
-                    ship.image = rot_center(original,ship.angle)
-                elif event.key == pygame.K_SPACE:
-                    bullet = Bullet()
-                    bullet.angle = ship.angle
-                    bullet.rect.x = ship.rect.x
-                    bullet.rect.y = ship.rect.y
-                    bullet_list.add(bullet)
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                bullet = Bullet()
+                bullet.angle = ship.angle
+                bullet.rect.x = ship.rect.x + (ship.image.get_width() / 2)
+                bullet.rect.y = ship.rect.y
+                bullet_list.add(bullet)
+
+        if keys[pygame.K_LEFT]:
+            ship.angle += 2
+            ship.image = rot_center(original,ship.angle)
+        elif keys[pygame.K_RIGHT]:
+            ship.angle -= 2
+            ship.image = rot_center(original,ship.angle)
+
+
 
         screen.fill(WHITE)
-        screen.blit(ship.image,(500,500))
+        screen.blit(ship.image,(400,400))
 
         bullet_list.update()
+        asteroid_list.update()
+        for b in bullet_list:
+            if b.rect.y < -10:
+                bullet_list.remove(b)
+            elif b.rect.y > 1010:
+                bullet_list.remove(b)
+            elif b.rect.x > 1004:
+                bullet_list.remove(b)
+            elif b.rect.x < -4:
+                bullet_list.remove(b)
+
+
         bullet_list.draw(screen)
-        Asteroid = load_image('Asteroid.png')
-        screen.blit(Asteroid, (900,500))
-        screen.blit(Asteroid, (100,500))
-        screen.blit(Asteroid, (500,100))
-        screen.blit(Asteroid, (500,900))
-        # pygame.draw.line(screen,RED,[500,500],[525,525],5)
-        # pygame.draw.line(screen,RED,[500,500],[475,525],5)
-        # pygame.draw.line(screen,RED,[525,525],[475,525],5)
+        asteroid_list.draw(screen)
+        # screen.blit(ast.image,(100,100))
+
+        # ast = load_image('Asteroid.png')
+        # screen.blit(ast, (400,500))
+        # screen.blit(ast, (100,500))
+        # screen.blit(ast, (500,100))
         pygame.display.flip()
         clock.tick(60)
     pygame.quit()
